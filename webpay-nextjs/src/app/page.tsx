2d7cfd5a213980296b3d4a475695bfb761e36564
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2024-04-24 17:23:37 -0400

    agregado nextjs
    Signed-off-by: figonzal1 <felipe.gonzalezalarcon94@gmail.com>

---
+++ webpay-nextjs/src/app/page.tsx
@@ -0,0 +1,66 @@
+"use client";
+import React, { useState } from "react";
+import { Button, Input } from "@nextui-org/react";
+import axios from "axios";
+
+export default function Home() {
+  const [tx, setTx] = useState({
+    token: "",
+    url: "",
+  });
+  const [price, setPrice] = useState(0);
+
+  const createTx = () => {
+    const runPost = async () => {
+      const result = await axios.post("http://localhost:3000/webpay", {
+        amount: price,
+      });
+
+      setTx(result.data);
+    };
+
+    runPost();
+  };
+
+  return (
+    <main className="min-h-screen">
+      <div className="flex flex-col items-center justify-start p-24 gap-10">
+        <h1 className="font-bold text-2xl text-center">
+          Ganatiempo Transbank - Webpay Test
+        </h1>
+
+        <div className="flex flex-col justify-center items-center gap-4">
+          <Input
+            type="number"
+            placeholder="0.00"
+            label="Precio"
+            className="max-w-xs"
+            onChange={(event) => setPrice(Number.parseInt(event.target.value))}
+          />
+
+          <Button
+            className="bg-blue-600 rounded-lg py-3 px-5 font-bold text-white"
+            onClick={() => createTx()}
+          >
+            Iniciar Tx
+          </Button>
+
+          {tx.token && tx.url && (
+            <div
+              dangerouslySetInnerHTML={{
+                __html: `
+                <form method="post" action="${tx.url}">
+                  <input type="hidden" name="token_ws" value="${tx.token}" />
+                  <button style="background-color: #F97316; border-radius: 0.5rem; padding: 0.75rem 1.25rem;" type="submit">
+                    Pagar
+                  </button>
+                </form>
+              `,
+              }}
+            />
+          )}
+        </div>
+      </div>
+    </main>
+  );
+}
---
+++ webpay-nextjs/src/app/result/success/page.tsx
@@ -0,0 +1,14 @@
+const Success = () => {
+  return (
+    <>
+      <div className="min-h-screen flex flex-col justify-center items-center gap-3">
+        <h1 className="text-2xl font-bold">Estado del pago transbank</h1>
+        <div className="bg-green-500 py-3 px-5 rounded-xl">
+          <h1 className="font-bold">Pago existoso</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Success;
